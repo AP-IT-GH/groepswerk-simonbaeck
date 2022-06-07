9eb5355c5b59e1e502f8142e82a4baf79a84dbce
Simon Baeck <simon.baeck@student.ap.be>
2022-06-07 20:22:38 +0200

    Added asset part8

---
+++ Assets/Scripts/DetectHit.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class DetectHit : MonoBehaviour
+{
+    public Turret turretAgent;
+    private GameObject Spawner;
+    private GameObject Turret;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawner = GameObject.FindGameObjectWithTag("Spawner");
+        Turret = GameObject.FindGameObjectWithTag("TurretFoot");
+        turretAgent = Turret.GetComponent<Turret>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        /* if (other.gameObject.tag == "Bullet")
+        {
+            Destroy(other.gameObject);
+            
+            if (gameObject.tag == "TargetHitZone")
+            {
+                Debug.Log("<b><color=green>Target</color></b>");
+                turretAgent.SetReward(1f);
+                //turretAgent.scoreText.text = turretAgent.GetCumulativeReward().ToString();
+                //turretAgent.scoreText.color = Color.green;
+                turretAgent.EndEpisode();
+            } 
+            
+            if (gameObject.tag == "Muur")
+            {
+                Debug.Log("<b><color=red>Muur</color></b>");
+                turretAgent.SetReward(-0.15f);
+                //turretAgent.scoreText.text = turretAgent.GetCumulativeReward().ToString();
+                //turretAgent.scoreText.color = Color.red;
+                //turretAgent.EndEpisode();
+            }
+        } */
+
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Laser")
+        {
+            if (gameObject.tag == "TargetHitZone")
+            {
+                Debug.Log("<b><color=green>AI HIT</color></b>");
+                turretAgent.SetReward(1f);
+                //turretAgent.scoreText.text = turretAgent.GetCumulativeReward().ToString();
+                //turretAgent.scoreText.color = Color.red;
+                turretAgent.EndEpisode();
+            }
+        }
+
+        if (other.gameObject.tag == "Bullet")
+        {
+            if (gameObject.tag == "TargetHitZone")
+            {
+                Debug.Log("<b><color=green>Player HIT</color></b>");
+                //turretAgent.scoreText.text = turretAgent.GetCumulativeReward().ToString();
+                //turretAgent.scoreText.color = Color.red;
+                turretAgent.EndEpisode();
+            }
+        }
+    }
+}
