1a59ba1bbd0a40ba4b3bf79b212ca67768a7329a
Piet B <bolspiet@gmail.com>
2022-06-08 18:55:11 +0200

    Auto stash before checking out "HEAD"

--- Assets/Scripts/LineRendererSettings.cs
+++ Assets/Scripts/LineRendererSettings.cs
@@ -8,7 +8,11 @@ public class LineRendererSettings : MonoBehaviour
     [SerializeField] LineRenderer rend;
     public LayerMask layerMask;
     public GameObject panel;
+<<<<<<< Updated upstream
     public Image img;
+=======
+    public Image[] imgs;
+>>>>>>> Stashed changes
     public Button btn;
 
     Vector3[] points;
@@ -16,7 +20,12 @@ public class LineRendererSettings : MonoBehaviour
     private void Start()
     {
         rend = gameObject.GetComponent<LineRenderer>();
+<<<<<<< Updated upstream
         img = panel.GetComponent<Image>();
+=======
+        //imgs = panel.GetComponent<Image>();
+        imgs[0] = gameObject.GetComponent("start_button").gameObject.GetComponent<Image>();
+>>>>>>> Stashed changes
         points = new Vector3[2];
         points[0] = Vector3.zero;
         points[1] = transform.position + new Vector3(0,0,20);
@@ -45,8 +54,13 @@ public class LineRendererSettings : MonoBehaviour
         {
             points[1] = transform.forward + new Vector3(0, 0, hit.distance);
             btn = hit.collider.gameObject.GetComponent<Button>();
+<<<<<<< Updated upstream
             rend.startColor = Color.green;
             rend.endColor = Color.green;
+=======
+            rend.startColor = Color.yellow;
+            rend.endColor = Color.yellow;
+>>>>>>> Stashed changes
             hitBtn = true;
         }
         else
@@ -64,6 +78,7 @@ public class LineRendererSettings : MonoBehaviour
     {
         if(btn != null)
         {
+<<<<<<< Updated upstream
             if(btn.name == "red_button")
             {
                 img.color = Color.red;
@@ -75,6 +90,26 @@ public class LineRendererSettings : MonoBehaviour
             else if(btn.name == "green_button")
             {
                 img.color = Color.green;
+=======
+            if(btn.name == "start_button")
+            {
+                imgs[0].color = Color.blue;
+            }
+            else if(btn.name == "sound_button")
+            {
+                if(imgs[0].color != Color.red)
+                {
+                    imgs[0].color= Color.red;
+                }
+                else
+                {
+                    imgs[0].color = Color.green;
+                }
+            }
+            else if(btn.name == "exit_button")
+            {
+                imgs[0].color = Color.clear;
+>>>>>>> Stashed changes
             }
         }
     }
